91468df5152a02e00bef22058fa339b3675b5f76
Magnus <magnus.thor.ar@gmail.com>
2018-10-19 14:30:31 +0200

    allow all network calls outside the @api_call tag

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,7 @@ Coveralls.wear_merged!('rails')
 require 'cucumber/rails'
 require 'webmock/cucumber'
 
+WebMock.allow_net_connect!
 ActionController::Base.allow_rescue = false
 
 World(FactoryBot::Syntax::Methods)
@@ -32,16 +33,17 @@ Capybara.register_driver :selenium do |app|
 end
 
 Cucumber::Rails::Database.javascript_strategy = :truncation
-WebMock.disable_net_connect!(allow_localhost: true)
+
 Before '@api_call' do 
+  WebMock.disable_net_connect!(allow_localhost: true)
   stub_request(:get, /www.food2fork.com/).
   with(
     headers: {
-	  'Accept'=>'*/*',
-	  'Accept-Encoding'=>'gzip, deflate',
-	  'Content-Type'=>'application/json',
-	  'Host'=>'www.food2fork.com',
-	  'User-Agent'=>'rest-client/2.0.2 (darwin17.7.0 x86_64) ruby/2.5.1p57'
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip, deflate',
+          'Content-Type'=>'application/json',
+          'Host'=>'www.food2fork.com',
+          'User-Agent'=>'rest-client/2.0.2 (linux-gnu x86_64) ruby/2.5.1p57'
     }).
   to_return(status: 200, body: Rails.root.join('features', 'support', 'fixtures', 'recipes_collection.txt').read, headers: {})
 end
